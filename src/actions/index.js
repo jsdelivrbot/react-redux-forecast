9fdf344a51f8994ab8b0175255edcda198003bb1
Edgar Zavala <ezavile@gmail.com>
2017-01-30 23:18:51 -0600

    adding SearchBar component

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+const API_KEY = '2b1dc17fd934f11aa6d0f726873677b5';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
